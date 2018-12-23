928e3ff44252789487fe3448cb33349c574e414e
Cam Askew <askewc@gmail.com>
2018-12-22 20:03:14 -0800

    actually send the request to the server!

--- frontend/main.js
+++ frontend/main.js
@@ -6,23 +6,37 @@ function updateWebCamImage() {
     requestAnimationFrame(updateWebCamImage);
 }
 
-let previousLeft = 0;
-let previousRight = 0;
+let previousLeft = undefined;
+let previousRight = undefined;
 
 function moveRobot() {
     if (connected) {
         const gamepad = navigator.getGamepads()[0];
-        const left = Math.round(10 * gamepad.axes[1]) / 10;
-        const right = Math.round(10 * gamepad.axes[3]) / 10;
+        const left = Math.round(10 * gamepad.axes[1]) / -10;
+        const right = Math.round(10 * gamepad.axes[3]) / -10;
 
         if (previousLeft !== left || previousRight !== right) {
-            console.log(left, right);
+            // noinspection JSIgnoredPromiseFromCall
+            fetch('api/move', {
+                method: 'POST',
+                cache: 'no-cache',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    left: left,
+                    right: right,
+                }),
+            }).then(moveRobot);
+
             previousLeft = left;
             previousRight = right;
+        } else {
+            requestAnimationFrame(moveRobot);
         }
+    } else {
+        requestAnimationFrame(moveRobot);
     }
-
-    requestAnimationFrame(moveRobot);
 }
 
 window.addEventListener('gamepadconnected', (e) => {
