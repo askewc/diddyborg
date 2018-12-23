e65f85caa868c2e97532671f5e0b7df464713ecd
Cam Askew <askewc@gmail.com>
2018-12-22 16:59:44 -0800

    Auto update web cam preview in frontend.

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+const webCamImage = document.getElementById('web-cam');
+
+function updateWebCamImage() {
+    webCamImage.src = 'cam.jpg?cache_buster=' + Date.now();
+    requestAnimationFrame(updateWebCamImage)
+}
+
+updateWebCamImage();
