904c03da94e0522d129ceaa68c1cddfa2b7dac85
Cam Askew <askewc@gmail.com>
2018-12-22 19:43:53 -0800

    Just log the axes...

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,35 @@
 const webCamImage = document.getElementById('web-cam');
+let axes;
 
-function update() {
+function updateWebCamImage() {
     webCamImage.src = 'cam.jpg?cache_buster=' + Date.now();
-    requestAnimationFrame(update);
+    requestAnimationFrame(updateWebCamImage);
 }
 
-update();
+function moveRobot() {
+    if (axes) {
+        console.log(axes);
+    }
+
+    requestAnimationFrame(moveRobot);
+}
+
+window.addEventListener('gamepadconnected', (e) => {
+    console.log('Gamepad connected!');
+
+    const gamepad = navigator.getGamepads()[0];
+
+    axes = {
+        left: gamepad.axes[1],
+        right: gamepad.axes[3],
+    };
+});
+
+
+window.addEventListener('gamepaddisconnected', (e) => {
+    console.log('Gamepad disconnected!');
+    axes = undefined;
+});
+
+updateWebCamImage();
+moveRobot();
